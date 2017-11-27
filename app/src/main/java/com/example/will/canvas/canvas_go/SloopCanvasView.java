d346e250b371c5e8099002d104ed4278c8c14e4d
Will <will.g.zhou@magugi.com>
2017-11-27 10:57:29 +0800

    画布的一些相关操作

--- app/src/main/java/com/example/will/canvas/canvas_go/SloopCanvasView.java
+++ app/src/main/java/com/example/will/canvas/canvas_go/SloopCanvasView.java
@@ -16,6 +16,7 @@ public class SloopCanvasView extends View {
     private Paint mPaint;
     private int mBlack = 0xffff0000;
     private int mGreen = 0xff00ff00;
+    private int mWidth, mHigh;
 
     public SloopCanvasView(Context context) {
         this(context, null);
@@ -39,15 +40,61 @@ public class SloopCanvasView extends View {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHigh = h;
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.translate(100, 100);
-        canvas.drawCircle(0, 0, 50, mPaint);
+        /**
+         * 位移
+         */
+//        canvas.translate(100, 100);
+//        canvas.drawCircle(0, 0, 50, mPaint);
+//
+//        mPaint.setColor(mGreen);
+//        canvas.translate(300, 300);
+//        canvas.drawCircle(0, 0, 200, mPaint);
 
+
+//        canvas.translate(mWidth / 2, mHigh / 2);
+//
+//        RectF rectF = new RectF(0, 0, 400, 400);
+//        canvas.drawRect(rectF, mPaint);
+//
+//        mPaint.setColor(mGreen);
+//        canvas.scale(-0.5f,-0.5f,100,0);
+//        canvas.drawRect(rectF,mPaint);
+
+        /**
+         * 缩放叠加小案例
+         */
+//        canvas.translate(mWidth / 2, mHigh / 2);
+//        RectF rectF = new RectF(-700, -700, 700, 700);
+//        mPaint.setStyle(Paint.Style.STROKE);
+//        for (int i = 0; i < 70; i++) {
+//            canvas.scale(0.95f, 0.95f);
+//            canvas.drawRect(rectF, mPaint);
+//        }
+
+
+        /**
+         * rotate 小案例
+         */
+        canvas.translate(mWidth / 2, mHigh / 2);
+        canvas.drawCircle(0, 0, 500, mPaint);
         mPaint.setColor(mGreen);
-        canvas.translate(300, 300);
-        canvas.drawCircle(0, 0, 200, mPaint);
+        canvas.drawCircle(0, 0, 400, mPaint);
+
+        for (int i = 0; i < 20; i++) {
+            canvas.drawLine(0, 400, 0, 500, mPaint);
+            canvas.rotate(20);
+        }
+
 
     }
 }
